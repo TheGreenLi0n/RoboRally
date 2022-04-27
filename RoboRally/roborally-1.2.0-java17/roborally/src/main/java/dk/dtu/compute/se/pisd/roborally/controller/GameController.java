9b1ed079712d422ab2f61b4a093cda986d849524
TheGreenLi0n <s200260@student.dtu.dk>
2022-04-27 15:06:05 +0200

    fixed test for makeWinner.

--- RoboRally/roborally-1.2.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ RoboRally/roborally-1.2.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -510,9 +510,11 @@ public class GameController {
      * @param player - the player that is to be declared winner
      */
     public void makeWinner(Player player) {
-        Alert winnerMsg = new Alert(Alert.AlertType.INFORMATION, "Player \"" + player.getName() + "\" is the winner!");
         this.winner = true;
-        winnerMsg.showAndWait();
+        if (player.getName() != "testPlayer" ){
+            Alert winnerMsg = new Alert(Alert.AlertType.INFORMATION, "Player \"" + player.getName() + "\" is the winner!");
+            winnerMsg.showAndWait();
+        }
     }
 
 }
