65f6278337900dd94935911a3fbe3fc8c9640429
Joakim Anker Kruse <joakimkruse@hotmail.com>
2022-06-06 14:38:13 +0200

    Changes by GreenLion

--- RoboRally/roborally-1.2.0-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/AppControllerTest.java
+++ RoboRally/roborally-1.2.0-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/AppControllerTest.java
@@ -1,12 +1,46 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.RoboRally;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppControllerTest {
 
+
+    private AppController appController;
+
+
+    @BeforeEach
+    void setUp() {
+        RoboRally roboRally = new RoboRally();
+        appController = new AppController(roboRally);
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        appController = null;
+    }
+
     @Test
     void newGame() {
+        appController.newGame();
+        appController.stopGame();
+
     }
+
+    @Test
+    void saveGame(){
+
+    }
+
+    @Test
+    void loadGame(){
+
+    }
+
+
 }
